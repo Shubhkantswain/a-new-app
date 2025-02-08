2776d60ccc896f7dd1ecef2002b08baba062767d
shubhswain45 <shubhkantswain@.com>
2025-02-08 20:16:58 +0530

    ppp

--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,16 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
-import "./tailwind.css";
+export const loader = async () => {
+  return json({ message: "Welcome to the SSR page in Remix!" });
+};
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export default function Index() {
+  const data = useLoaderData<typeof loader>();
 
-export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <div>
+      <h1>{data.message}</h1>
+    </div>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
